6fd8634fbc900071acadc745d9800beca6134e02
Caleb Morris <caleb.morris.g@gmail.com>
2014-11-28 23:42:04 -0700

    Added tests for authentication

--- tests/test.js
+++ tests/test.js
@@ -62,22 +62,80 @@ describe('Unauthorized State', function() {
 });
 
 describe('authenticate', function() {
-  var requestStub;
-
-  before(function(done) {
-    requestStub = sinon.stub(request, 'post', function(options, callback) {
-      callback(null, { statusCode : 200 },
-        '{ "token_type" : "bearer", "access_token" : "token" }');
-    });
-    done();
+  afterEach(function() {
+    request.post.restore();
   });
 
-  after(function(done) {
-    request.post.restore();
-    done();
+  it('Empty Response', function(done) {
+    var requestStub = sinon.stub(request, 'post', function(options, callback) {
+      callback(null, { statusCode : 200 });
+    });
+    var twit = new (injectTwitterStubs(requestStub))('apiKey', 'apiSecret');
+    twit.authenticate()
+    .then(done.bind(null, 'Promise should reject'))
+    .catch(function(err) {
+      expect(err).to.be.instanceOf(Error);
+      expect(err.message).to.contain('body');
+      expect(requestStub.callCount).to.equal(1);
+      done();
+    })
+    .catch(done);
+  });
+
+  it('Missing response properties', function(done) {
+    var requestStub = sinon.stub(request, 'post', function(options, callback) {
+      callback(null, { statusCode : 200 }, '{}');
+    });
+    var twit = new (injectTwitterStubs(requestStub))('apiKey', 'apiSecret');
+    twit.authenticate()
+    .then(done.bind(null, 'Promise should reject'))
+    .catch(function(err) {
+      expect(err).to.be.instanceOf(Error);
+      expect(err.message).to.contain('token');
+      expect(requestStub.callCount).to.equal(1);
+      done();
+    })
+    .catch(done);
+  });
+
+  it('404', function(done) {
+    var requestStub = sinon.stub(request, 'post', function(options, callback) {
+      var response = { statusCode : 404 };
+      callback(null, response);
+    });
+    var twit = new (injectTwitterStubs(requestStub))('apiKey', 'apiSecret');
+    twit.authenticate()
+    .then(done.bind(null, 'Promise should reject'))
+    .catch(function(err) {
+      expect(err).to.be.instanceOf(Error);
+      expect(err.message).to.contain('bearer');
+      expect(requestStub.callCount).to.equal(1);
+      done();
+    })
+    .catch(done);
+  });
+
+  it('500', function(done) {
+    var requestStub = sinon.stub(request, 'post', function(options, callback) {
+      callback(null, { statusCode : 500 });
+    });
+    var twit = new (injectTwitterStubs(requestStub))('apiKey', 'apiSecret');
+    twit.authenticate()
+    .then(done.bind(null, 'Promise should reject'))
+    .catch(function(err) {
+      expect(err).to.be.instanceOf(Error);
+      expect(err.message).to.contain('bearer');
+      expect(requestStub.callCount).to.equal(1);
+      done();
+    })
+    .catch(done);
   });
 
   it('success', function(done) {
+    var requestStub = sinon.stub(request, 'post', function(options, callback) {
+      callback(null, { statusCode : 200 },
+        '{ "token_type" : "bearer", "access_token" : "token" }');
+    });
     var twit = new (injectTwitterStubs(requestStub))('apiKey', 'apiSecret');
     twit.authenticate()
     .then(done)
