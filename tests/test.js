19c219bc1d0f6ca924fda0787db6c4459064a435
Caleb Morris <caleb.morris.g@gmail.com>
2014-11-28 22:57:01 -0700

    Added a test for authentication and mocked out IO

--- tests/test.js
+++ tests/test.js
@@ -1,32 +1,38 @@
-/* global describe, it, beforeEach, afterEach*/
+/* global describe, it, before, beforeEach, after, afterEach*/
 'use strict';
 
-var expect = require('chai').expect;
-var assert = require('chai').assert;
+var expect     = require('chai').expect;
+var proxyquire = require('proxyquire');
+var request    = require('request');
+var sinon      = require('sinon');
 
-var Twitter = require('../index');
+function injectTwitterStubs(requestStub) {
+  return proxyquire('../index', {
+    request : requestStub || {},
+  });
+}
 
 describe('Constructor', function() {
   it('should throw error for missing API Key', function() {
     expect(function() {
-      return new Twitter();
+      return new (injectTwitterStubs())();
     }).to.throw(TypeError);
   });
 
   it('should throw error for missing API Secret', function() {
     expect(function() {
-      return new Twitter('apiKey');
+      return new (injectTwitterStubs())('apiKey');
     }).to.throw(TypeError);
   });
 
   it('should throw error for invalid API Key', function() {
     expect(function() {
-      return new Twitter(1);
+      return new (injectTwitterStubs())(1);
     }).to.throw(TypeError);
   });
 
   it('should succeed with valid API key and secret', function() {
-    var twit = new Twitter('apiKey', 'apiSecret');
+    var twit = new (injectTwitterStubs())('apiKey', 'apiSecret');
     expect(twit).to.be.an('object');
     expect(twit.access_token).to.equal(undefined);
   });
@@ -35,14 +41,14 @@ describe('Constructor', function() {
 describe('Unauthorized State', function() {
   var twit;
   beforeEach(function() {
-    twit = new Twitter('apiKey', 'apiSecret');
+    twit = new (injectTwitterStubs())('apiKey', 'apiSecret');
   });
 
   afterEach(function() {
     twit = undefined;
   });
 
-  it('should throw error', function(done) {
+  it('getTweets', function(done) {
     twit.getTweets({})
     .then(function() {
       done(new Error('getTweets should throw an error when unauthorized'));
@@ -51,6 +57,30 @@ describe('Unauthorized State', function() {
       expect(err).to.be.a('boolean');
       done();
     })
+    .catch(done);
+  });
+});
+
+describe('authenticate', function() {
+  var requestStub;
+
+  before(function(done) {
+    requestStub = sinon.stub(request, 'post', function(options, callback) {
+      callback(null, { statusCode : 200 },
+        '{ "token_type" : "bearer", "access_token" : "token" }');
+    });
+    done();
+  });
+
+  after(function(done) {
+    request.post.restore();
+    done();
+  });
+
+  it('success', function(done) {
+    var twit = new (injectTwitterStubs(requestStub))('apiKey', 'apiSecret');
+    twit.authenticate()
+    .then(done)
     .catch(done);
   });
 });
