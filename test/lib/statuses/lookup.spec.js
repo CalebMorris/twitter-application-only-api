732c1275ee4a293205fad03562ee0657d83cbc0c
Caleb Morris <caleb.morris.g@gmail.com>
2020-11-22 16:49:33 -0700

    Added remaining changes to last commit (status/lookup)
    Not sure what happened, but me pushing the previous code didn't push all
     my changes

--- test/lib/statuses/lookup.spec.js
+++ test/lib/statuses/lookup.spec.js
@@ -9,11 +9,19 @@ describe('statuses/lookup', function() {
 
   describe('optionsSchema', function() {
 
-    const validationResponse = lookup.optionsSchema.validate({});
-    expect(validationResponse, JSON.stringify(validationResponse)).to.not.be.null;
-    expect(validationResponse.error, JSON.stringify(validationResponse.error)).to.not.be.null;
-    expect(validationResponse.error).to.be.instanceOf(ValidationError);
-    expect(validationResponse.error.message).to.contain('"id" is required')
+    it('should fail when missing required fields', () => {
+      const validationResponse = lookup.optionsSchema.validate({});
+      expect(validationResponse, JSON.stringify(validationResponse)).to.not.be.null;
+      expect(validationResponse.error, JSON.stringify(validationResponse.error)).to.not.be.null;
+      expect(validationResponse.error).to.be.instanceOf(ValidationError);
+      expect(validationResponse.error.message).to.contain('"id" is required');
+    })
+
+    it('should pass when all required fields', () => {
+      const validationResponse = lookup.optionsSchema.validate({id:['id1']});
+      expect(validationResponse, JSON.stringify(validationResponse)).to.not.be.null;
+      expect(validationResponse.error, JSON.stringify(validationResponse.error)).to.be.undefined;
+    })
 
   })
 
